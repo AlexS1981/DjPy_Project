a3528ccbdb07a7ad10616ac2ca9763cb0944c27d
Alex <frstl@ukr.net>
2020-11-05 18:02:26 +0200

    002

--- proj_001/app_001/models.py
+++ proj_001/app_001/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
+    test_field = models.TextField(blank=True)
 
     def __str__(self):
         return str(self.name)
